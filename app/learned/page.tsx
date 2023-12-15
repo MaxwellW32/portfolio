1216e9d81eec66bf8d1db3f007d627ee77e0975e
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-12-14 21:33:08 -0500

    test

--- app/learned/page.tsx
+++ app/learned/page.tsx
@@ -7,62 +7,14 @@ import eyeShadow from "../../public/LearnedStuff/eye shadow.png"
 import eyeOutline from "../../public/LearnedStuff/eye outline only.png"
 
 function Learned() {
+  const eyeContRef = useRef<HTMLDivElement>(null!)
+  const realEyeRef = useRef<HTMLImageElement>(null!)
+  const anchorRef = useRef<HTMLDivElement>(null!)
 
-  useEffect(()=>{
+  useEffect(() => {
     //move eye with cursor
-    const eyeCont = document.querySelector("#eyeCont") as HTMLElement
-    const realEye = document.querySelector("#realeye") as HTMLElement
-    const anchor = document.querySelector("#anchor") as HTMLElement
-    const testT = document.querySelector("#testText") as HTMLElement
     let eyeMiddleFocused = false
-
-    const moveEyes = (event: MouseEvent)=>{
-      const {clientX: mouseX, clientY: mouseY} = event
-
-      const rekt = anchor.getBoundingClientRect()
-      const anchorX = rekt.left + rekt.width / 2
-      const anchorY = rekt.top + rekt.height / 2
-
-      let distanceX = anchorX-mouseX
-      let distanceY = anchorY-mouseY
-
-      if (distanceX < 0){
-        distanceX *= -1
-      }
-
-      if (distanceY < 0){
-        distanceY *= -1
-      }
-
-      let limit = 70
-      if (distanceX < limit && distanceY < limit){
-        eyeMiddleFocused = true
-
-        realEye.style.transition = `translate 1s`
-        realEye.style.translate = `0%`
-
-
-      }else{
-
-        if (eyeMiddleFocused){
-          realEye.style.translate = `-5%`
-
-          setTimeout(()=>{
-            realEye.style.transition = `translate 400ms`
-          },1000)
-
-          eyeMiddleFocused = false
-        }
-
-              const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
-        
-              eyeCont.style.transform = `rotate(${angleDeg}deg)`
-              realEye.style.rotate = `${angleDeg*-1}deg`
-      }
-
-    }
-
-    const angle = (cx:number, cy:number, ex:number, ey:number)=>{
+    const angle = (cx: number, cy: number, ex: number, ey: number) => {
       const dy = ey - cy
       const dx = ex - cx
       const rad = Math.atan2(dy, dx)
@@ -71,23 +23,67 @@ function Learned() {
 
     }
 
+    const moveEyes = (event: MouseEvent) => {
+      const { clientX, clientY } = event
+
+      const rekt = anchorRef.current.getBoundingClientRect()
+      const anchorX = rekt.left + rekt.width / 2
+      const anchorY = rekt.top + rekt.height / 2
+
+      let distanceX = anchorX - clientX
+      let distanceY = anchorY - clientY
+
+      if (distanceX < 0) {
+        distanceX *= -1
+      }
+
+      if (distanceY < 0) {
+        distanceY *= -1
+      }
+
+      let limit = 70
+      if (distanceX < limit && distanceY < limit) {
+        eyeMiddleFocused = true
+
+        realEyeRef.current.style.transition = `translate 1s`
+        realEyeRef.current.style.translate = `0%`
+
+      } else {
+
+        if (eyeMiddleFocused) {
+          realEyeRef.current.style.translate = `-5%`
+
+          setTimeout(() => {
+            realEyeRef.current.style.transition = `translate 400ms`
+          }, 1000)
+
+          eyeMiddleFocused = false
+        }
+
+        const angleDeg = angle(clientX, clientY, anchorX, anchorY)
+
+        realEyeRef.current.style.rotate = `${-angleDeg}deg`
+        eyeContRef.current.style.rotate = `${angleDeg}deg`
+      }
+
+    }
+
     document.addEventListener("mousemove", moveEyes)
 
-    return ()=>document.removeEventListener("mousemove", moveEyes)
-  },[])
+    return () => document.removeEventListener("mousemove", moveEyes)
+  }, [])
 
 
   return (
     <div className={styles.LearnDiv}>
 
-      <div className={styles.myCont} id="anchor">
-        <div className={styles.eyeCont} id="eyeCont">
-          <Image alt="eye" className={styles.realeye} id="realeye" src={eye}/>
+      <div className={styles.eyeMainDiv} ref={anchorRef} style={{ width: "300px", aspectRatio: "1/1", position: "relative", overflow: "hidden" }}>
+        <div ref={eyeContRef} style={{ position: "absolute", aspectRatio: "1/1", width: "100%", top: 0, left: 0 }}>
+          <Image ref={realEyeRef} alt="eye" src={eye} style={{ position: "relative", aspectRatio: "1/1", width: "100%", objectFit: "cover", top: 0, left: 0 }} />
         </div>
-        <Image alt="eyeshadow" src={eyeShadow}/>
-        <Image alt="eyeoutline" src={eyeOutline}/>
+        <Image style={{ position: "absolute", aspectRatio: "1/1", width: "100%", objectFit: "cover", top: 0, left: 0 }} alt="eyeshadow" src={eyeShadow} />
+        <Image style={{ position: "absolute", aspectRatio: "1/1", width: "100%", objectFit: "cover", top: 0, left: 0 }} alt="eyeoutline" src={eyeOutline} />
       </div>
-
 
       <section className={styles.hidden}>
         <h1 id="htmlStart">HTML</h1>
